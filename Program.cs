2d21d1ee5763ccdb24a64a5c44dea02d7e7965f6
Evgeniy Troitskiy <T89266455873@yandex.ru>
2022-07-05 14:28:06 +0400

    Финиш

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿/*int FindQuart(int x, int y)
+{
+    if(x>0 && y>0)return 1;
+    if(x<0 && y>0)return 2;
+    if(x<0 && y<0)return 3;
+    if(x>0 && y<0)return 4;
+    return -1;
+}
+
+Console.Write("Imput x coordinate: ");
+int x0=Convert.ToInt32(Console.ReadLine());
+Console.Write("Imput y coordinate: ");
+int y0=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Num of current quart is " + FindQuart(x0,y0));*/
+
+/*void findRange(int Quarter)
+{
+    if(Quarter == 1) Console.WriteLine("x-положительный, y-положительный");
+    if(Quarter == 2) Console.WriteLine("x-отрицательный, y-положительный");
+    if(Quarter == 3) Console.WriteLine("x-отрицательный, y-отрицательный");
+    if(Quarter == 4) Console.WriteLine("x-положительный, y-отрицательный");
+}
+Console.Write("Imput quarter: ");
+int Quarter=Convert.ToInt32(Console.ReadLine());
+findRange(Quarter);*/
+
+
+/*int ShowSquare(int N)
+{
+    int count = 1;
+    while(count<=N);
+    {
+        int squad=count*count;
+        Console.WriteLine(squad);
+        count++;
+    }
+}
+Console.WriteLine("imput number");
+Convert.ToInt32(Console.ReadLine());*/
+
+
+
+
+
